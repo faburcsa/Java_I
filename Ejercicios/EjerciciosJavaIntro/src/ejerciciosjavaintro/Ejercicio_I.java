e4e5036001d29981f91b652a02eb1f385708a8c9
faburcsa <fabianurchueguia@gmail.com>
2023-03-07 21:22:10 -0300

    test poner java

--- Ejercicios/EjerciciosJavaIntro/src/ejerciciosjavaintro/Ejercicio_I.java
+++ Ejercicios/EjerciciosJavaIntro/src/ejerciciosjavaintro/Ejercicio_I.java
@@ -5,10 +5,31 @@
  */
 package ejerciciosjavaintro;
 
+import java.util.Scanner; // Este es el importe de la clase Scanner
+
 /**
  *
  * @author fabia
  */
 public class Ejercicio_I {
-    
+    public static void main(String[] args) {
+        System.out.println("Ejercicio Nro. 1");  
+        /*Escribir un programa que pida dos números enteros por teclado 
+        y calcule la suma de los dos.
+        El programa deberá después mostrar el resultado de la suma*/
+        
+        Scanner leer = new java.util.Scanner(System.in); // Creamos el Scanner
+        
+        System.out.println("Ingrese un numero entero");
+        int numEntero = leer.nextInt(); // Leer un entero
+        
+        System.out.println("Ingrese otro numero entero");
+        int numEntero2 = leer.nextInt(); // Leer un entero2
+        
+        int suma=numEntero+numEntero2;
+        System.out.println("La suma de los numeros "+ numEntero + " + "+numEntero2+ " es "+ suma);
+         
+    }
+            
+            
 }
