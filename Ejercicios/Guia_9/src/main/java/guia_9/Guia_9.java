aeea078b2c222071314db1ff141fa3aa42b95f7d
faburcsa <fabianurchueguia@gmail.com>
2023-04-12 19:14:21 -0300

    Inicio Guia 9

---
+++ Ejercicios/Guia_9/src/main/java/guia_9/Guia_9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package guia_9;
+
+/**
+ *
+ * @author Fabian M. Urchueguia
+ */
+public class Guia_9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
