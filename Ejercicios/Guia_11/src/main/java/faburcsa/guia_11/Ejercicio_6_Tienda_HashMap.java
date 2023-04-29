4e955796051eac9b861021b9c09a94b925e6244d
faburcsa <fabianurchueguia@gmail.com>
2023-04-29 00:11:51 -0300

    Guia 11 - Ejercicio  6 - TIenda

--- Ejercicios/Guia_11/src/main/java/faburcsa/guia_11/Ejercicio_6_Tienda_HashMap.java
+++ Ejercicios/Guia_11/src/main/java/faburcsa/guia_11/Ejercicio_6_Tienda_HashMap.java
@@ -4,6 +4,9 @@
  */
 package faburcsa.guia_11;
 
+import java.util.Scanner;
+import servicios.ServicioProducto;
+
 /**
  *
  * @author Fabian M. Urchueguia
@@ -11,6 +14,8 @@ package faburcsa.guia_11;
  * Se necesita una aplicación para una tienda en la cual queremos almacenar los distintos
 productos que venderemos y el precio que tendrán. Además, se necesita que la
 aplicación cuente con las funciones básicas.
+* 
+* 
 Estas las realizaremos en el servicio. Como, introducir un elemento, modificar su precio,
 eliminar un producto y mostrar los productos que tenemos con su precio (Utilizar
 Hashmap). El HashMap tendrá de llave el nombre del producto y de valor el precio.
@@ -22,7 +27,47 @@ public class Ejercicio_6_Tienda_HashMap {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        
+        String opcion="S";
+        do {
+            System.out.println("MENU TIENDA:");
+            System.out.println("1 - Agregar Producto");
+            System.out.println("2 - Eliminar Producto");
+            System.out.println("3 - Modificar Precio Producto");
+            System.out.println("4 - Mostrar Productos");
+            System.out.println("5 - Salir");
+            
+            opcion = leer.next();
+            switch (opcion) {
+                case "1":
+                    ServicioProducto.agregarHashProductos(ServicioProducto.crearProducto());
+                    opcion="S";
+                    break;
+                case "2":
+                    ServicioProducto.eliminarProductos();
+                    opcion="S";
+                    break;
+                case "3":
+                    ServicioProducto.modificaPrecio();
+                    opcion="S";
+                    break;
+                case "4":
+                    ServicioProducto.mostrarHashProductos();
+                    opcion="S";
+                    break;
+                 case "5":
+                    opcion="N";
+                    break;
+                default:
+                    System.out.println("Opcion Incorrecta, reintente...");
+                    opcion="S";
+            }
+        } while (opcion.substring(0, 1).equalsIgnoreCase("S"));
+        
+        
+        
+        
     }
     
 }
--- Ejercicios/Guia_11/src/main/java/servicios/ServicioProducto.java
+++ Ejercicios/Guia_11/src/main/java/servicios/ServicioProducto.java
@@ -4,10 +4,88 @@
  */
 package servicios;
 
+import entidades.Producto;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  *
  * @author Fabian M. Urchueguia
+ *
+ * Estas las realizaremos en el servicio. Como, introducir un elemento,
+ * modificar su precio, eliminar un producto y mostrar los productos que tenemos
+ * con su precio (Utilizar Hashmap). El HashMap tendrá de llave el nombre del
+ * producto y de valor el precio. Realizar un menú para lograr todas las
+ * acciones previamente mencionadas.
  */
 public class ServicioProducto {
-    
+
+    public static Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    public static HashMap<String, Double> hashProductos = new HashMap();
+
+    public static Producto crearProducto() {
+        System.out.println("Ingrese Nombre Producto:");
+        String nombre = leer.next();
+
+        System.out.println("Ingrese Precio Producto:");
+        Double precio = leer.nextDouble();
+
+        Producto p = new Producto(nombre, precio);
+
+        System.out.println("Desea Agregar Descripcion el producto " + p.getNombre() + " S/N?");
+        if (leer.next().substring(0, 1).equalsIgnoreCase("S")) {
+            System.out.println("Ingrese descripcion:");
+            p.setDescripcion(leer.next());
+        }
+
+        System.out.println("Desea Agregar Cantidad del producto " + p.getNombre() + " S/N?");
+        if (leer.next().substring(0, 1).equalsIgnoreCase("S")) {
+            System.out.println("Ingrese cantidad disponible:");
+            p.setCantidad(leer.nextInt());
+        }
+
+        return p;
+    }
+
+    public static void agregarHashProductos(Producto p) {
+        hashProductos.put(p.getNombre(), p.getPrecio());
+    }
+
+    public static void mostrarHashProductos() {
+        for (Map.Entry<String, Double> entry : hashProductos.entrySet()) {
+            Object key = entry.getKey();
+            Object val = entry.getValue();
+            System.out.println("Producto: " + key + " - Precio $" + val);
+        }
+    }
+
+    public static void eliminarProductos() {
+        System.out.println("LISTA ACTUAL:");
+        mostrarHashProductos();
+        System.out.println("");
+        System.out.println("Ingrese nombre de producto a eliminar:");
+        hashProductos.remove(leer.next());
+
+        System.out.println("LISTA DE PRODUCTOS ACTUALIZADA!!!");
+    }
+
+    public static void modificaPrecio() {
+        System.out.println("LISTA ACTUAL:");
+        mostrarHashProductos();
+        System.out.println("");
+        System.out.println("Ingrese nombre de producto para modificar precio:");
+
+        for (Map.Entry<String, Double> entry : hashProductos.entrySet()) {
+            Object key = entry.getKey();
+            Object val = entry.getValue();
+            if (key.equals(leer.next())) {
+                System.out.println("Ingrese Nuevo precio:");
+                Double newPrice=leer.nextDouble();
+                hashProductos.replace((String) key,newPrice );
+            }
+        }
+
+        System.out.println("LISTA DE PRODUCTOS ACTUALIZADA!!!");
+    }
 }
